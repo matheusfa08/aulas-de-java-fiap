2a9fbc96525669ffea0933a503ad64aea4ef1b1e
matheusfa08 <littlepacoquinha@gmail.com>
2026-03-12 09:45:00 -0300

    Criação/Instaciação de objetos

---
+++ src/Livro.java
@@ -0,0 +1,26 @@
+public class Livro {
+        //Atributos: caracteristicas do objeto
+        //Padrão é camelCase, sempre utilize!
+        //tipo + nome_atributo
+
+        String tituloLivro;
+
+        int dataPublicacao;
+
+        String nomeAutora;
+
+        String editora;
+
+        String fotoCapa;
+
+        String resumoLivro;
+
+        double notaAvaliacao;
+
+        int paginas;
+
+        String tipo_capa;
+
+        double preco;
+
+}
---
+++ src/TesteLivro.java
@@ -0,0 +1,9 @@
+public class TesteLivro {
+    static void main(String[] args){
+        //Criar objetos - instanciar
+        Livro meuLivro = new Livro();
+        meuLivro.tituloLivro = "Harry Pohha: e a Pedra de Crakus";
+        meuLivro.nomeAutora = "M.A. Theus";
+        meuLivro.editora = "PP"
+    }
+}
